6c8be485ad7d01257cba1b8209666c31a6939750
Nathan Soucier <soucienp@gmail.com>
2024-08-07 16:14:47 -0400

    updating users test file

--- routes/users.test.js
+++ routes/users.test.js
@@ -1,20 +1,13 @@
-
-
-// old
-
 "use strict";
 
 /** Routes for users. */
-
-const jsonschema = require("jsonschema");
-
 const express = require("express");
-const { ensureCorrectUserOrAdmin, ensureAdmin } = require("../middleware/auth");
-const { BadRequestError } = require("../expressError");
+// const { ensureCorrectUserOrAdmin, ensureAdmin } = require("../middleware/auth");
+// const { BadRequestError } = require("../expressError");
 const User = require("../models/user");
-const { createToken } = require("../helpers/tokens");
-const userNewSchema = require("../schemas/userNew.json");
-const userUpdateSchema = require("../schemas/userUpdate.json");
+// const { createToken } = require("../helpers/tokens");
+// const userNewSchema = require("../schemas/userNew.json");
+// const userUpdateSchema = require("../schemas/userUpdate.json");
 
 const router = express.Router();
 
